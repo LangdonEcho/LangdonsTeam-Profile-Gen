a01d207d5510ece3f0694b7f21ba0f743b5a37e7
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-06 04:30:23 -0600

    starting the index.js files

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const fs = require('fs');
+const generateTeam = require('./src/generateTeam');
+
+// Employee array
+let employeesArr = [];
+
+// Questions array for all employees
+const questions = [           
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of this employee?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's this employee's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What role does this employee have?",
+        choices: ["Engineer", "Intern", "Manager"]
+    }
+    ]
