18ccc19aaf770a0e6ccfb98a37e5b7b3bd22d09f
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-07 04:44:13 -0600

    adding some working code to starter files

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,15 @@
+const Employee = require('./Employee');
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        // Get methods from Employee class
+        super(name, id, email);
+        getGithub() {
+            return this.github
+        }
+    
+        getRole() {
+            return 'Engineer'
+        }
+    }
+    
+    module.exports = Engineer;
