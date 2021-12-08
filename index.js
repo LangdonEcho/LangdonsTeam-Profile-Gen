6d2f58f5709f371b16f829263140c7d5744221a2
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-07 19:28:13 -0600

    adding a lot to index.js

--- index.js
+++ index.js
@@ -4,31 +4,60 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const fs = require('fs');
 const generateTeam = require('./src/generateTeam');
+const generateHTML = require('./src/generate.html');
 
 // Employee array
-let employeesArr = [];
+const employees = [];
 
 // Questions array for all employees
-const questions = [           
-    {
-        type: "input",
-        name: "name",
-        message: "What is the name of this employee?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What's this employee's ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the employee's email?"
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "What role does this employee have?",
-        choices: ["Engineer", "Intern", "Manager"]
-    }
-    ]
+team = [];
+const managerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'provide the manager name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'provide the team manager id',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'provide the team manager email?',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },       
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'provide the team manager office number?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'stop adding team members'],
+        }
+    ])
+    const writeToFile = data => {
+        fs.writeToFile('./dist/index.html', data, err => {
+            // if there is an error 
+            if (err) {
+                console.log(err);
+                return;
+            // when the profile has been created 
+            } else {
+                console.log("Your team profile has been successfully created! Please check out the index.html")
+            }
+        })
+    }; 
